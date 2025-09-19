7d52b742fd4c7d30a0685d22a1e02d0c74a7a744
sammasterd <samuelmurcia333@gmail.com>
2025-09-19 17:47:58 +0200

    Update evil.js

--- evil.js
+++ evil.js
@@ -5,8 +5,9 @@
   style.innerHTML = `
     #cdn-poison-alert {
       position: fixed;
-      top: 20px;
-      right: 20px;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%)
       z-index: 999999;
       background: #111;
       color: #f00;
